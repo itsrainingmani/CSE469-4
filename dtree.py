3c43eef4f775e3685495613e8faf21e2d13a5ea8
Manikandan <mtiruman@buffalo.edu>
2015-11-27 17:36:29 -0500

    Latest

--- dtree.py
+++ dtree.py
@@ -1,22 +1,16 @@
 import csv
 
-def initialgini(ddata, label):
+def initialgini(data, label):
     numPass = 0
     tSize = 0
     safety= 0
 
 data = []
 
-with open("car.csv", "rb") as csvfile:
+with open("golf.csv", "rb") as csvfile:
     golf = csv.reader(csvfile, delimiter= ',')
     for row in golf:
         data.append(row)
 
-ddata = []
-
-ddata.append([data[i][0] for i in xrange(14)])
-ddata.append([data[i][1] for i in xrange(14)])
-ddata.append([data[i][2] for i in xrange(14)])
-ddata.append([data[i][3] for i in xrange(14)])
-
-label = ["unacc", "acc", "good", "vgood"]
+#label = ["unacc", "acc", "good", "vgood"]
+label = ["yes","no"]
